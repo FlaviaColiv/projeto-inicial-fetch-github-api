5780674461e94bfad22fe34562e103cde911d43d
Flavia Oliveira <flaviacoliv@hotmail.com>
2025-03-02 06:11:01 -0300

    jsAvanc 020325 17

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,22 @@
-async function user(){
-    const response = await fetch('https://api.github.com/users/FlaviaColiv')
+document.getElementById('btn-search').addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value
+    getUserProfile(userName)
+})
+
+async function user(userName){
+    const response = await fetch(`https://api.github.com/users/${userName}`)
     return await response.json()
 }
+
+function getUserProfile(userName){
+    user(userName).then(userData => {
+        let userInfo =`<img src="${userData.avatar_url}" alt="Foto do Perfil do Usuário" />
+                        <div class="data">
+                            <h1>${userData.name ?? 'Não possui nome cadastrado 🥲'}</h1>
+                            <p>${userData.bio ?? 'Não possui bio cadastrada 🥲'}</p>
+                        </div>`
+
+        document.querySelector('.profile-data').innerHTML = userInfo
+    })
+}
+
